f3c2c5f30290e6e2c9f84c935147e3ef6046aa7e
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-03-09 10:47:09 -0500

    Form.jsx

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { TextField,Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
-
-
+import useStyles from "./Styles";
 
 const Form = () => {
+    const classes = useStyles();
+
   return (
-    <div>Form</div>
+    <Grid container spacing={2}>
+        <Grid item xs={12}>
+            <Typography align="center" variant="subtitle2" gutterBottom>
+                ...
+            </Typography>
+        </Grid>
+        <Grid item xs={6}>
+            <FormControl></FormControl>
+        </Grid>
+    </Grid>
   )
 }
 
