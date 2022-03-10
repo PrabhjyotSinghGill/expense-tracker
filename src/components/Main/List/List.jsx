af0fb1a5d6c9b955ef87d7d72a94ea8d723efa67
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-03-10 15:19:23 -0500

    ListItemAvatar

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -5,13 +5,22 @@ import useStyles from "./styles";
 const List = () => {
     const classes = useStyles();
     const transactions = [
-        {id:1,}
+        {id:1,type:"Income"}
     ];
   return (
     <MUIList dense={false} className={classes.list}>
         {transactions.map((transaction) =>(
             <Slide direction='down' in mountOnEnter unmountOnExit key={transaction.id}>
-
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                            <MoneyOff></MoneyOff>
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText>
+                        
+                    </ListItemText>
+                </ListItem>
             </Slide>
         ) )}    
     </MUIList>
